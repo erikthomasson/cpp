09f18901d877304fa29ff1e30c112f9c5094541c
erikthomasson <erik.thomasson98@hotmail.com>
2019-04-28 14:15:29 +0200

    done?

--- Laboration_2_Studentfiler/VS Code/Includes/AVLTree.hpp
+++ Laboration_2_Studentfiler/VS Code/Includes/AVLTree.hpp
@@ -9,110 +9,353 @@ template <typename T>
 class AVLTree
 {
   private:
+	Node* insert(Node *nextNode, T &newVal);
+	Node* remove(Node *nextNode, T &delVal);
+	Node* rightBalancing(Node *node);
+	Node* leftBalancing(Node *node);
+	Node* maxValuedelete(Node* node);
+	T postOrderWalk(Node*node);
+	T inOrderWalk(Node*node);
+	T preOrderWalk(Node*node);
 	struct Node
 	{
 		T value;
-		size_t height=0;
+		size_t height = -1;
 		Node *rightChild = nullptr;
 		Node *leftChild = nullptr;
 	};
-	Node *root;
+	Node *root = nullptr;
 
   public:
+	AVLTree();
 	void insert(T element);
 	void remove(T element);
-	bool find(T element);
+	bool find(T element); 
 	std::vector<T> preOrderWalk();
 	std::vector<T> inOrderWalk();
 	std::vector<T> postOrderWalk();
-	size_t getTreeHeight();
-	T getMin();
-	T getMax();
-	std::string ToGraphviz()
+	size_t getTreeHeight(); 
+	T getMin();				
+	T getMax();				
+	
+
+  private: 
+	T postOrderWalk(Node* node){
+		if(node ==nullptr){
+			return;
+		}
+		postOrderWalk(node->leftChild);
+		postOrderWalk(node->rightChild);
+		return node->value;
+
+	}
+	T inOrderWalk(Node* node){
+		if(node ==nullptr){
+			return;
+		}
+		inOrderWalk(node->leftChild);
+		return node->value;
+		inOrderWalk(node->rightChild);
+	}
+	T preOrderWalk(Node* node){
+		if(node ==nullptr){
+			return;
+		}
+		return node->value;
+		preOrderWalk(node->leftChild);
+		preOrderWalk(node->rightChild);
+	}
+
+	Node* maxValuedelete(Node* node){
+		Node* temp = node;
+		while(temp->rightChild != nullptr){
+			temp = temp->rightChild;
+		}
+		return temp;
+	}
+
+	Node *insert(Node *node, T &newVal)
+	{
+		//add element
+		if (node == nullptr)
+		{
+			node->value = element;
+			newNode->height = 1;
+		}
+
+		if (newVal < node->value)
+			node->left = insert(node->left, newVal);
+		else if (key > node->key)
+			node->right = insert(node->right, newVal);
+		else
+		{
+			// Equal values are not allowed
+			throw "Equal values are not allowed";
+			return node;
+		}
+
+		//fix the height
+		if (node->leftChild->height > node->rightChild->height)
+		{
+			node->height = node->leftChild->height + 1;
+		}
+		else
+		{
+			node->height = node->rightChild->height + 1;
+		}
+		
+		if ((nextNode->rightChild->height - nextNode->leftChild->height) > 1 && nextNode->height > -1)
+			return leftBalancing(nextNode);
+		else if (((nextNode->leftChild->height - nextNode->rightChild->height) > 1) && nextNode->height > -1)
+			return rightBalancing(nextNode);
+
+		return node;
+	}
+
+	Node *rightBalancing(Node *node)
+	{
+		//rotation till höger
+		Node *tempL = node->leftChild;
+		if((tempL->rightChild->height > tempL->leftChild->height){
+			//dubbel rotation
+			Node *tempLeftRight = tempL->rightChild;
+			if (tempLeftRight->leftChild != nullptr)
+			{
+				node->rightChild = tempLeftRight->leftChild;
+			}
+			else
+			{
+				node->rightChild = nullptr;
+			}
+			tempLeftRight->leftChild = node;
+			if (tempLeftRight->rightChild != nullptr)
+			{
+				tempL->leftChild = tempLeftRight->rightChild;
+				tempLeftRight->rightChild = tempL;
+			}
+			else
+			{
+				tempLeftRight->rightChild = tempL;
+				tempL->leftChild = nullptr;
+			}
+
+			//height adjustments
+			if (node->rightChild->height > node->leftChild->height)
+			{
+				node->height = node->rightChild->height + 1;
+			}
+			else
+			{
+				node->height = node->leftChild->height + 1;
+			}
+			if (tempL->rightChild->height > tempL->leftChild->height)
+			{
+				tempL->height = tempL->rightChild->height + 1;
+			}
+			else
+			{
+				tempL->height = tempL->leftChild->height + 1;
+			}
+			if (tempLeftRight->rightChild->height > tempLeftRight->leftChild->height)
+			{
+				tempLeftRight->height = tempLeftRight->rightChild->height + 1;
+			}
+			else
+			{
+				tempLeftRight->height = tempLeftRight->leftChild->height + 1;
+			}
+			return tempLeftRight;
+		
+	}
+
+	else{
+			//singel rotation
+			if (tempL->rightChild != nullptr)
+			{
+				node->leftChild = tempL->rightChild;
+				tempL->rightChild = node;
+			}
+			else
+			{
+				tempL->rightChild = node;
+				node->leftChild = nullptr;
+			}
+
+			// height adjustment
+			if (node->rightChild->height > node->leftChild->height)
+			{
+				node->height = node->rightChild->height + 1;
+			}
+			else
+			{
+				node->height = node->leftChild->height + 1;
+			}
+			if (tempL->rightChild->height > tempL->leftChild->height)
+			{
+				tempL->height = tempL->rightChild->height + 1;
+			}
+			else
+			{
+				tempL->height = tempL->leftChild->height + 1;
+			}
+			return tempL;
+		}
+	}
+
+	Node *leftBalancing(Node *node)
+	{
+		//rotation till vänster
+		Node *tempR = node->rightChild;
+		if((tempR->leftChild->height > tempR->rightChild->height){
+			//dubbel rotation
+			Node *tempRightLeft = tempR->leftChild;
+			if (tempRightLeft->leftChild != nullptr)
+			{
+				node->rightChild = tempRightLeft->leftChild;
+			}
+			else
+			{
+				node->rightChild = nullptr;
+			}
+			tempRightLeft->leftChild = node;
+			if (tempRightLeft->rightChild != nullptr)
+			{
+				tempR->leftChild = tempRightLeft->rightChild;
+				tempRightLeft->rightChild = tempR;
+			}
+			else
+			{
+				tempRightLeft->rightChild = tempR;
+				tempR->leftChild = nullptr;
+			}
+
+			// height adjustment
+			if (node->rightChild->height > node->leftChild->height)
+				node->height = node->rightChild->height + 1;
+			else
+				node->height = node->leftChild->height + 1;
+			if (tempR->rightChild->height > tempR->leftChild->height)
+				tempR->height = tempR->rightChild->height + 1;
+			else
+				tempR->height = tempR->leftChild->height + 1;
+			if (tempRightLeft->rightChild->height > tempRightLeft->leftChild->height)
+				tempRightLeft->height = tempRight->rightChild->height + 1;
+			else
+				tempRightLeft->height = tempRightLeft->leftChild->height + 1;
+			return tempRightLeft;
+	}
+	else{
+			//singel rotation
+			if (tempR->leftChild != nullptr)
+			{
+				node->rightChild = tempR->leftChild;
+				tempR->leftChild = node;
+			}
+			else
+			{
+				tempR->leftChild = node;
+				node->rightChild = nullptr;
+			}
+	
+			// height adjustment
+			if (node->rightChild->height > node->leftChild->height)
+				node->height = node->rightChild->height + 1;
+			else
+				node->height = node->leftChild->height + 1;
+			if (tempR->rightChild->height > tempR->leftChild->height)
+				tempR->height = tempR->rightChild->height + 1;
+			else
+				tempR->height = tempR->leftChild->height + 1;
+			return tempR;
+	}
+	}
+
+
+	Node *remove(Node *node, T &delVal)
+	{
+		
+		if (node == nullptr)
+		{
+			throw "Empty Tree";
+			return node;
+			//throwa tomt träd
+		}
+
+		if (delVal < node->value)
+			node->leftChild = remove(node->leftChild, delVal);
+		else if (delVal > node->value)
+			node->rightChild = remove(node->rightChild, delVal);
+		else
+		{
+			// noll barn
+			if(node->leftChild == nullptr && node->rightChild==nullptr){
+				delete node;
+				return nullptr;	
+			}
+			// 1 barn
+			else if(node->leftChild == nullptr){
+				Node *temp = node->rightChild;
+				delete node;
+				return temp;
+
+			}
+			else if(node->rightChild == nullptr){
+				Node *temp = node->letChild;
+				delete node;
+				return temp; 
+			}
+			else {
+				//2 barn
+				Node* temp = maxValuedelete(node->leftChild);
+				node->value = temp->value;
+				node->leftChild = remove(node->leftChild, temp->value);
+			}
+
+			return node;
+		}
+
+		//fix the height
+
+		if (node->leftChild->height > node->rightChild->height)
+		{
+			node->height = node->leftChild->height + 1;
+		}
+		else
+		{
+			node->height = node->rightChild->height + 1;
+		}
+		//balance
+		if ((nextNode->rightChild->height - nextNode->leftChild->height) > 1 && nextNode->height > -1)
+			return leftBalancing(nextNode);
+		else if (((nextNode->leftChild->height - nextNode->rightChild->height) > 1) && nextNode->height > -1)
+			return rightBalancing(nextNode);
+
+		return node;
+	}
 };
+
+template <typename T>
+AVLTree<T>::AVLTree()
+{
+	Node *newNode = new Node;
+	newNode->rightChild = nullptr;
+	newNode->leftChild = nullptr;
+	newNode->height = -1; 
+	root = newNode;
+}
 
 template <typename T>
 void AVLTree<T>::insert(T element)
 {
-	//1: lägg till element
-	//2: balansera trädet 
 	Node *temp = root;
-	void traversing(Node* nextNode,bool &insert = true ,T &newVal = element){
-		if( ((this->rightChild->height - this->leftChild->height) > 1) || ((this->leftChild== nullptr) && this->rightChild > 1)){
-			//rotation till vänster
-			
-			Node *tempV = this->rightChild;
-			if((tempV->leftChild > tempV->rightChild){
-				//dubbel rotation
-			}
-			else{
-				//singel rotation 
-				if (tempV->leftChild != nullptr){
-					this->rightChild = tempV->leftChild;
-					tempV->leftChild = this;
-					if (this->leftChild->height < this->rightChild->height) this->height = this->rightChild->height + 1;
-				}
-				else{
-					tempV->leftChild = this; 
-					this->rightChild = nullptr;
-					this->height = 0;
-				}
-				
-			}
-			
-		}
-		if( ((this->leftChild->height - this->rightChild->height) > 1) || ((this->rightChild == nullptr) && this->leftChild > 1)){
-			//rotation till höger
-			
-			Node *tempR = this->leftChild;
-			if((tempR->rightChild > tempR->leftChild){
-				//dubbel rotation
-			}
-			else{
-				//singel rotation
-				if(tempR->rightChild != nullptr){
-				}
-				else{
-
-				}
-			}
-		}
-		if(element<this->value && this->leftChild != nullptr){
-			return traversing(this->leftChild, false);
-		}
-		else if (element>this->value && this->rightChild != nullptr){
-			return traversing(this->rightChild,false);
-		}
-		else{
-			//de e lika så throwa
-		}
-
-
-		if(element<this->value && this->leftChild == nullptr){
-			Node newNode* = new Node;
-			newNode->value = element;
-			
-		}
-		else if(element>this->value && this->rightChild == nullptr){
-			Node newNode* = new Node;
-			newNode->value = element;
-		
-		}
-		else {
-			//de e lika så throwa
-		}
-		return;
-
-	}
+	insert(temp, element);
 }
 
-
 template <typename T>
-void AVLTree<T>::remove(T element) {
+void AVLTree<T>::remove(T element)
+{
 	Node *temp = root;
-	void traversing(Node* nextNode,bool &insert = true ,T &newVal = element){
-
-	}
+	remove(temp, element);
 }
 
 template <typename T>
@@ -141,18 +384,30 @@ bool AVLTree<T>::find(T element)
 }
 
 template <typename T>
-std::vector<T> AVLTree<T>::preOrderWalk() {}
+std::vector<T> AVLTree<T>::preOrderWalk() {
+	std::vector<T> vect;
+	vect.push_back(preOrderWalk(this->root));
+	return vect;
+}
 
 template <typename T>
-std::vector<T> AVLTree<T>::inOrderWalk() {}
+std::vector<T> AVLTree<T>::inOrderWalk() {
+	std::vector<T> vect;
+	vect.push_back(inOrderWalk(this->root));
+	return vect;
+}
 
 template <typename T>
-std::vector<T> AVLTree<T>::postOrderWalk() {}
+std::vector<T> AVLTree<T>::postOrderWalk() {
+	std::vector<T> vect;
+	vect.push_back(postOrderWalk(this->root));
+	return vect;
+}
 
 template <typename T>
 size_t AVLTree<T>::getTreeHeight()
 {
-	return this->height;
+	return root->height;
 }
 
 template <typename T>
@@ -176,9 +431,6 @@ T AVLTree<T>::getMax()
 	}
 	return temp->value;
 }
-
-
-
 
 /*
 
