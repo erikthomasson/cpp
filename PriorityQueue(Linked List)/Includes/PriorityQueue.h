9275a897989984ea393a9656b4a75b78f1144066
erikthomasson <erik.thomasson98@hotmail.com>
2019-05-13 11:16:58 +0200

    priorityQkompl

--- PriorityQueue(Linked List)/Includes/PriorityQueue.h
+++ PriorityQueue(Linked List)/Includes/PriorityQueue.h
@@ -4,113 +4,120 @@
 #include <utility>
 #include <iterator>
 
-
 template <typename T>
-class PriorityQueue{
+class PriorityQueue
+{
     //A template Priority Queue class by Erik Thomasson. Priority on the least valuable variable (1<2 or a<b ...)
-    private:
-        size_t cap=0;
-        struct Node{
-            T value;
-            Node *nextNode = nullptr;
-        };
-        Node *first = nullptr, *back= nullptr;
-    public:
-        PriorityQueue() = default;
-        ~PriorityQueue();
-        void enqueue(const T &element);
-        void dequeue();
-        T peek() const;
-        size_t size() const;
-        bool isEmpty() const;
+private:
+    size_t cap = 0;
+    struct Node
+    {
+        T value;
+        Node *nextNode = nullptr;
+    };
+    Node *first = nullptr, *back = nullptr;
 
+public:
+    PriorityQueue() = default;
+    ~PriorityQueue();
+    void enqueue(const T &element);
+    void dequeue();
+    T peek() const;
+    size_t size() const;
+    bool isEmpty() const;
 };
 
 template <typename T>
-PriorityQueue<T>::~PriorityQueue(){
-    if(first != nullptr){
-    Node *deleteNodes = first->nextNode;
-    while (deleteNodes->nextNode != nullptr)
+PriorityQueue<T>::~PriorityQueue()
+{
+    if (first != nullptr)
     {
+        Node *deleteNodes = first->nextNode;
+        while (deleteNodes->nextNode != nullptr)
+        {
+            delete first;
+            first = deleteNodes;
+            deleteNodes = deleteNodes->nextNode;
+        }
         delete first;
-        first = deleteNodes;
-        deleteNodes = deleteNodes->nextNode;
-    }
-    delete first;
     }
 }
 
 template <typename T>
-void PriorityQueue<T>::enqueue(const T &element){
+void PriorityQueue<T>::enqueue(const T &element)
+{
     //Adds a new element in the queue
     Node *newNode = new Node;
     newNode->value = element;
     newNode->nextNode = nullptr;
-    if(first == nullptr){
+    if (first == nullptr)
+    {
         first = newNode;
         back = newNode;
     }
-    else{
-        Node *temp= first;
-        if(newNode->value >= back->value){
+    else
+    {
+        Node *temp = first;
+        if (back->value <  newNode->value)
+        {
             back->nextNode = newNode;
             back = newNode;
         }
         else if (newNode->value < first->value)
         {
             newNode->nextNode = first;
-            first=newNode;
+            first = newNode;
         }
-        else{
-            std::cout<<temp->nextNode->value<<std::endl;
-            std::cout<<newNode->value<<std::endl;
-            
-            while(temp->nextNode->value < newNode->value){
+        else
+        {
+            std::cout << temp->nextNode->value << std::endl;
+            std::cout << newNode->value << std::endl;
+
+            while (temp->nextNode->value < newNode->value)
+            {
                 temp = temp->nextNode;
             }
-            
+
             newNode->nextNode = temp->nextNode;
             temp->nextNode = newNode;
-
         }
-
     }
     cap++;
 }
 
 template <typename T>
-void PriorityQueue<T>::dequeue(){
+void PriorityQueue<T>::dequeue()
+{
     //dequeue the first element in the queue
-    try{
-    if(cap < 1){
+    if (cap < 1)
+    {
         throw "ERROR: The Queue is empty";
     }
     Node *temp = first->nextNode;
     delete first;
     first = temp;
     cap--;
-    }catch (const std::exception &e){
-        std::cerr<<e.what()<<"\n";
-    }
 }
 
 template <typename T>
-T PriorityQueue<T>::peek() const{
+T PriorityQueue<T>::peek() const
+{
     //Returns the first element in the queue
-    if(this->isEmpty()) throw "ERROR: The Queue is empty";
-        return first->value;
-   
+    if (this->isEmpty())
+        throw "ERROR: The Queue is empty";
+    return first->value;
 }
 
 template <typename T>
-size_t PriorityQueue<T>::size() const{
+size_t PriorityQueue<T>::size() const
+{
     //Function that returns the size of the queue
     return this->cap;
 }
 
 template <typename T>
-bool PriorityQueue<T>::isEmpty() const{
+bool PriorityQueue<T>::isEmpty() const
+{
     //Function that return if the queue is empty or not
     return this->cap == 0;
 }
-                                    
--- Testfiler/main.cpp
+++ Testfiler/main.cpp
@@ -1,32 +1,72 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include </home/erik/c++/cpp/Laboration_1_Studentfiler/Includes/PriorityQueue.h>
+#include "/home/erik/c++/cpp/Testfiler/PriorityQ.hpp"
 #include <vector>
 #include <map>
+#include <limits.h>
 
-struct Node{
-    std::map<Node*,unsigned int> adj;
+struct Vertex{
+
+    std::map<Vertex&,int> adj;
+    int distance;
+    Vertex* path;
+    std::string name;
+    int operator<(Vertex& s){
+        if(this->distance<s.distance) return this->distance;
+        else return s.distance;
+    }
+    int operator>(Vertex& s){
+        if(this->distance>=s.distance) return this->distance;
+        else return s.distance;
+    }
 };
 
-
-int main(int argc , char* path[]){
-    //path[1] == sökvägen till existerande fil
-    //path[2] == billigaste rutten från A - B
-    std::string str = "path";
-    std::ifstream inFile;
-    std::ofstream outFile; 
-    std::map<std::string,Node*> nodes;
+void printPath(Vertex v, std::string& shortestPath){
+    if(v.path!=nullptr){
+        printPath(*v.path, shortestPath);
+    }
+    shortestPath+= (v.name + " -> ");
     
-    inFile.open(str);
+}
+void dijkstra(std::map<std::string,Vertex>& allVertex){
+    PriorityQueue<Vertex> q;
+    allVertex["start vertex"].distance=0;
+    
+    for(auto it = allVertex.begin();it != allVertex.end(); it++){
+        q.enqueue(it->second);
+    }
+    Vertex* vertexP = new Vertex;
+    while(!q.isEmpty()){
+        *vertexP= q.peek();
+        q.dequeue();
+        for ( auto it = vertexP->adj.begin(); it != vertexP->adj.end(); it++ ){
+            int dist = vertexP->distance + it->second;
+            if(dist < it->second){
+                it->first.distance=dist;
+                it->first.path=vertexP;
+            }
+        }
+    }
+    delete vertexP;
+}
+
+
+std::map<std::string, Vertex> readFile(std::string path2File){
+    std::ifstream inFile;
+    std::map<std::string,Vertex> allVertex;
+    inFile.open(path2File);
+
     if(inFile.is_open()){
         std::string val,val2,val3;
         std::getline(inFile,val,'\n');
         if(val == "DIRECTED"){
             std::getline(inFile, val, '\n');
             while(val != ""){
-                //Node* newNode = new Node;
-                nodes[val] = new Node;
+                Vertex newVertex;
+                newVertex.distance = INT_MAX;
+                newVertex.path = nullptr;
+                allVertex[val] = newVertex;
                 std::getline(inFile, val, '\n');
             }
             while(inFile.good()){
@@ -36,14 +76,150 @@ int main(int argc , char* path[]){
                 //till nod
                 std::getline(inFile, val3, '\t');
                 //vikt
-                nodes[val]->adj[nodes[val2]] = std::stoi(val3);
+                allVertex[val].name=val;
+                allVertex[val].adj[allVertex[val2]] = std::stoi(val3);
+                
             }
-        
         }
-        else if(val== "UNDIRECTED"){
+        else if(val=="UNDIRECTED"){
+            std::getline(inFile, val, '\n');
+            while(val != ""){
+                Vertex newVertex;
+                newVertex.distance = INT_MAX;
+                newVertex.path = nullptr;
+                allVertex[val] = newVertex;
+                std::getline(inFile, val, '\n');
+            }
+            while(inFile.good()){
+                std::getline(inFile, val, '\t');
+                //från nod
+                std::getline(inFile, val2, '\t');
+                //till nod
+                std::getline(inFile, val3, '\t');
+                //vikt
+                
+                //Vertex A(val) ---weight(val3)---> Vertex B(val2)
+                allVertex[val].name=val;
+                allVertex[val].adj[allVertex[val2]] = std::stoi(val3);
+                
 
+                //Vertex B(val2) ---weight(val3)---> Vertex A(val)
+                allVertex[val2].name=val;
+                allVertex[val2].adj[allVertex[val]] = std::stoi(val3);
+                
+            }
+        }
+        else{
+            throw "Unvalid File";
+        }
+        return allVertex;
+    
+    }
+}
+void write2File(const std::map<std::string, Vertex>& allVertex, const int& distance, const std::string& shortestPath){
+    std::ofstream outFile;
+    outFile.open("Answer.txt");
+    outFile<<"0"<<"\n";
+    outFile<<distance<<"\n";
+    outFile<<shortestPath<<"\n";
+
+}
+
+
+int main(int argc , char* path[]){
+    //path[1] == sökvägen till existerande fil
+    //path[2] == billigaste rutten från A - B
+    std::string str=path[2], startV,endV;
+    bool diff=false;
+    for(int i=0;i<str.size();i++){
+        if(str[i]=='-') continue;
+        if(!diff) startV+=str[i];
+        else endV+=str[i];
+    }
+    std::string str = "path[1]";
+    std::ifstream inFile;
+    std::ofstream outFile; 
+    std::map<std::string,Vertex> allVertex;
+    
+    inFile.open(str);
+    if(inFile.is_open()){
+        std::string val,val2,val3;
+        std::getline(inFile,val,'\n');
+        if(val == "DIRECTED"){
+            std::getline(inFile, val, '\n');
+            while(val != ""){
+                Vertex newVertex;
+                newVertex.distance = INT_MAX;
+                newVertex.path = nullptr;
+                allVertex[val] = newVertex;
+                std::getline(inFile, val, '\n');
+            }
+            while(inFile.good()){
+                std::getline(inFile, val, '\t');
+                //från nod
+                std::getline(inFile, val2, '\t');
+                //till nod
+                std::getline(inFile, val3, '\t');
+                //vikt
+                allVertex[val].name=val;
+                allVertex[val].adj[allVertex[val2]] = std::stoi(val3);
+                
+            }
+            PriorityQueue<Vertex> q;
+            allVertex[startV].distance=0;
+            
+            for(auto it = allVertex.begin();it != allVertex.end(); it++){
+                q.enqueue(it->second);
+            }
+            Vertex* vertexP = new Vertex;
+            while(!q.isEmpty()){
+                *vertexP= q.peek();
+                q.dequeue();
+                for ( auto it = vertexP->adj.begin(); it != vertexP->adj.end(); it++ ){
+                    int dist = vertexP->distance + it->second;
+                    if(dist < it->second){
+                        it->first.distance=dist;
+                        it->first.path=vertexP;
+                    }
+                    }
+                }
+            delete vertexP;
+            }
+        else if(val== "UNDIRECTED"){
+            std::getline(inFile, val, '\n');
+            while(val != ""){
+                Vertex newVertex;
+                newVertex.distance = INT_MAX;
+                newVertex.path = nullptr;
+                allVertex[val] = newVertex;
+                std::getline(inFile, val, '\n');
+            }
+            while(inFile.good()){
+                std::getline(inFile, val, '\t');
+                //från nod
+                std::getline(inFile, val2, '\t');
+                //till nod
+                std::getline(inFile, val3, '\t');
+                //vikt
+                
+                //Vertex A(val) ---weight(val3)---> Vertex B(val2)
+                allVertex[val].name=val;
+                allVertex[val].adj[allVertex[val2]] = std::stoi(val3);
+                
+                //Vertex B(val2) ---weight(val3)---> Vertex A(val)
+                allVertex[val2].name=val;
+                allVertex[val2].adj[allVertex[val]] = std::stoi(val3);   
+            }
+        }
+        else{
+            throw "Unvalid File";
         }
     }
-
+    else{
+        throw "Wrong file path or the file doesnt exist";
+    }
+    std::string shortestPath;
+    printPath(allVertex[endV], shortestPath);
+    write2File(allVertex,allVertex[startV].distance,shortestPath);
 
 }
